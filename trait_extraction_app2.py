d869af9c27eea4a529ac07a8327e45c770ffd14a
Amy Fiddes <a.fiddes@kew.org>
2024-12-11 15:29:56 +0000

    Script to extract qualitative variables from appendix_2
    Still needs work. The LLM can't handle the entire appendix, therefore regex matches specific subjects defined by the user

---
+++ trait_extraction_app2.py
@@ -0,0 +1,49 @@
+import llm
+import pandas as pd
+import re
+
+# Choose model
+model = llm.get_model("groq-llama3.1-70b")
+
+# Read the files and enter the prompt
+with open("data/appendix_2.txt", "r") as appendix_2:
+    appendix_2 = appendix_2.read()
+
+with open("data/sentences_cat.txt", "r") as sentences_cat:
+    sentences_cat = sentences_cat.read()
+
+df_sentences = pd.read_csv("data/sentences_cat.txt")
+df_app2 = pd.read_csv("data/appendix_2.txt")
+
+for taxon_name in df_sentences.taxon_name.unique():
+    print(taxon_name)
+
+    taxon_dict = dict()
+
+    for subject in df_app2.subject.to_list():
+        # Print just the relevant columns
+        #print(df_app2[df_app2.subject==subject][["description", "code"]])
+        # Make a markdown table using those column headers and print
+        appendix_2_subject = df_app2[df_app2.subject==subject][["description", "code"]].to_markdown(index=False)
+        #print(appendix_2_subject)
+        # Iterate through the rows in df_sentences that match the current subject and taxon_name
+        for i, row in df_sentences[(df_sentences.category==subject) & (df_sentences.taxon_name==taxon_name)].iterrows():
+            sentence_subject = row["sentence"]
+
+            # Print the subjects for each sentence per taxon_name
+            #print(f"Current subject: {subject}")
+
+            if re.match('stems', subject):
+
+                prompt = f"""
+                    You are an expert botanist. We're interested in characteristics about {subject} we want to extract JSON format data as defined in the table below
+                    {appendix_2_subject}. Use abbreviation as your key. Your input sentence is "{sentence_subject}". Abbreviations in parentheses at the end of each variable are the values to be assigned to the subjects. The states of the variables here are scored as ‘(0)’ or ‘(1)’ etc.
+                    Do not fabricate data and ensure the values correspond to the correct abbreviation, if values for traits are not recored, leave blank. Respond only with JSON.
+                    """
+
+                response = model.prompt(prompt, temperature=0)
+                output = response.text()
+                print(output)
+
+
+# Need to fix so output isn't repeated lots of times (or just clean the output somehow)
