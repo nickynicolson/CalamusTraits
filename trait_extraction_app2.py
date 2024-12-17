5ca778205b97cd35c259d5d0e17c9723a2873b1b
Nicky Nicolson <nicky.nicolson@gmail.com>
2024-12-17 11:33:38 +0000

    Switch to use groq API library for app 2 extraction

--- trait_extraction_app2.py
+++ trait_extraction_app2.py
@@ -1,4 +1,6 @@
-import llm
+import os
+from groq import Groq
+import groq
 import pandas as pd
 import re
 import json
@@ -13,13 +15,12 @@ def count_tokens(prompt, model_name="gpt2"):
     return len(tokens)
 
 # Choose model
+model_name = "llama-3.3-70b-versatile"
 
-
-# model_name = "llama3.1-70b"
-model_name = "llama-3.3-70b"
-groq_model_name = f"groq-{model_name}"
-
-model = llm.get_model(groq_model_name)
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),  # GROQ API key is set as an environment variable
+    max_retries=5,
+)
 
 # Read the files and enter the prompt
 with open("data/appendix_2.txt", "r") as appendix_2:
@@ -51,14 +52,14 @@ for taxon_name in df_sentences.taxon_name.unique():
         batches = [df_app2_subject[i:i + batch_size] for i in range(0, len(df_app2_subject), batch_size)]
 
         for i, batch in enumerate(batches):
-            print(f"Batch {i + 1}:\n{batch}\n")
+            # print(f"Batch {i + 1}:\n{batch}\n")
             appendix_2_subject_batch = batch.to_markdown(index=False)
             # print(appendix_2_subject_batch)
 
             codes_len = len(batch)
+            # textwrap.dedent strips out the indenting spaces in the multline text string
             prompt_outline = textwrap.dedent("""
-                You are an expert botanist. You can extract and encode data from text to JSON. 
-                Your supplies: 
+                Your working materials: 
                 a description of a species ("description"), 
                 a markdown table containing codes for a trait ("code") and sets of rules ("rules") 
                 about the description values used to encode the trait. 
@@ -77,27 +78,43 @@ for taxon_name in df_sentences.taxon_name.unique():
                 {appendix_2_subject_batch}
                 """)
             prompt = prompt_outline.format(subject_para=subject_para, appendix_2_subject_batch=appendix_2_subject_batch)
-            print(prompt)
-            print(count_tokens(prompt))
-
-            response = model.prompt(prompt, temperature=0)
-            output = response.text()
-            print(output)
-
+            
             try:
+
+                chat_completion = client.chat.completions.create(
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": "You are an expert botanist. You can extract and encode data from text to JSON.",
+                        },
+                        {
+                            "role": "user",
+                            "content": prompt,
+                        },
+                    ],
+                    model=model_name,
+                )
+                output = chat_completion.choices[0].message.content
+
                 sentence_dict = json.loads(output)
-                print(f"Asked for {codes_len} codes")
-                print(f"Received {len(sentence_dict)} codes")
+                # print(f"Asked for {codes_len} codes")
+                # print(f"Received {len(sentence_dict)} codes")
                 if codes_len != len(sentence_dict):
                     print("Incomplete extraction" + '*'*60)
                 taxon_dict.update(sentence_dict)
-            except:
-                print(output)
-            
-            sleep(2)
+                sleep(10)
+            except groq.APIConnectionError as e:
+                print("The server could not be reached")
+                print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+            except groq.RateLimitError as e:
+                print("A 429 status code was received; we should back off a bit.")
+            except groq.APIStatusError as e:
+                print("Another non-200-range status code was received")
+                print(e.status_code)
+                print(e.response)
 
     df_taxon = pd.DataFrame([taxon_dict])            
     df_output = pd.concat([df_output, df_taxon])
 
 print(df_output)
-df_output.to_csv('data/etxracted_traits.csv', index=False)
+df_output.to_csv('data/extracted_traits.csv', index=False)
